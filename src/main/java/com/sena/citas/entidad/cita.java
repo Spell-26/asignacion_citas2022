de113350963c9bbac6e11de3057192e3e5a8dba3
viper2612 <jsc.2612@gmail.com>
2022-12-12 04:32:16 -0500

    se creo constructor y relaciones con las tablas necesarias para su contruccion (puede cambiar la estructura a futuro)

--- src/main/java/com/sena/citas/entidad/cita.java
+++ src/main/java/com/sena/citas/entidad/cita.java
@@ -10,5 +10,51 @@ public class cita {
     @Column(name = "cita_id")
     private int id;
 
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
+    private usuario cliente;
 
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "detalle_servicio_id")
+    private detalleServicios detalleS;
+
+    public cita() {
+    }
+
+    public cita(usuario cliente, detalleServicios detalleS) {
+        this.cliente = cliente;
+        this.detalleS = detalleS;
+    }
+
+    public cita(int id, usuario cliente, detalleServicios detalleS) {
+        this.id = id;
+        this.cliente = cliente;
+        this.detalleS = detalleS;
+    }
+
+    //getters y setters
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public usuario getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(usuario cliente) {
+        this.cliente = cliente;
+    }
+
+    public detalleServicios getDetalleS() {
+        return detalleS;
+    }
+
+    public void setDetalleS(detalleServicios detalleS) {
+        this.detalleS = detalleS;
+    }
 }
